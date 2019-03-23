3b83c9b9efb5388a3458d7734cf63bd05f24bc3c
hebermudezg <hebermudezg@unal.edu.co>
2019-03-23 11:11:39 -0500

    html

---
+++ scraping.R
@@ -0,0 +1,29 @@
+
+# tablas en html -----
+
+library(XML)
+url <- "../bases-de-datos/WorldPopulation-wiki.htm"
+tablas <- readHTMLTable(url, which = 6)
+tabla2 <- tablas[[2]]
+
+
+# no estructurados -----
+
+library(rvest)
+url <- "https://www.imdb.com/search/title?count=100&release_date=2016,2016&title_type=feature"
+
+peliculas <- read_html(url)
+nodos_tiulos <- html_nodes(peliculas,".lister-item-header a")
+titulos <- html_text(nodos_tiulos)
+
+
+# con tuberias 
+
+titulo2 <- peliculas %>% html_nodes(".lister-item-header a") %>% html_text()
+
+
+rank <- peliculas %>% html_nodes(".ratings-imdb-rating strong") %>% html_text() %>% as.numeric()
+
+
+
+
