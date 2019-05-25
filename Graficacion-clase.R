c66a278889bf9cfd4e92497b218b184636fe29bf
hebermudezg <hebermudezg@unal.edu.co>
2019-05-25 07:45:19 -0500

    actualizacion script graficacion

--- Graficacion-clase.R
+++ Graficacion-clase.R
@@ -100,8 +100,10 @@ hist(mpg)
 
 library(ggplot2)
 
-# scatter plot
 
+#### qplot ***************************************
+
+# scatter plot
 qplot(x = mpg ,y = weight ,data = auto, 
       geom = c("point", "smooth"), #method = "lm", se=F,
       xlab = "millas por galón",
@@ -119,23 +121,72 @@ qplot(y=mpg,x=  cylinders, geom = "boxplot")
 qplot(y=mpg,x=  cylinders, geom = "violin")
 
 
-## ggplot()
-ggplot(data = auto, aes(x=mpg, y = horsepower)) +
-  geom_point()+ geom_line(data = auto[1:10,], color ="red") 
+## ggplot() ********************************
+
+
+
+ggplot(data = auto, aes(x=mpg, y = horsepower)) + geom_point() +
+  geom_line(data = auto[1:10,], color="red")
+
+####
+ggplot(data = auto, aes(x=cylinders, y = horsepower)) +
+  geom_boxplot() + geom_dotplot(binaxis = "y", stackdir = "center", binwidth = 2) + 
+  stat_summary(fun.y = mean, geom = "point", color="red") 
+
+
+
+
+auto <- read.csv("https://raw.githubusercontent.com/unalyticsteam/bases-de-datos/master/auto-mpg.csv", stringsAsFactors = F)
+auto$cylinders <- factor(auto$cylinders)
+str(auto)
+attach(auto)
+
+
+# boxplot
+
+ggplot(data = auto, aes(x=cylinders, y = horsepower)) +
+  geom_violin() + labs(x="cilin", y="poder") # xlab("Cilindros") + ylab("poder")
+# 
+
+subset(auto)
+
+ggplot(data =auto, aes(x=horsepower, color = cylinders)) + geom_density()
+
+
+ggplot(data = auto, aes(x=mpg, y = horsepower, color=cylinders)) +
+  geom_point() + facet_grid(cylinders~.) +
+  scale_color_manual(values = c("#8CFFF1", "red", "blue", "#8CFFF1", "green" ))
+
+
+
+# 
+ggplot(data = diamonds, aes(x=carat, y = price)) + geom_hex()
 
 
 
 
 
+ggplot(data= auto, aes(x=acceleration)) + geom_histogram(aes(y=..density..)) +
+  geom_density(color = "red")
 
 
+# barras 
+
+ggplot(data=auto, aes(x=cylinders)) + geom_bar()
 
 
+##
 
+gapminder <- read.csv("https://raw.githubusercontent.com/unalyticsteam/bases-de-datos/master/gapminder.csv")
 
-
-
-
-
-
+a <- ggplot(data = gapminder, aes(x=gdpPercap, y=lifeExp, ids=country,
+                             colour = continent,
+                             size=pop,
+                             frame=year)) +
+  geom_point() + scale_x_log10() + facet_grid(continent~.)
+  labs(title=("Expectativa de vida vs Ingreso Percapita"))
+print(a)
+  
+library(plotly)
+ggplotly(a)
 
